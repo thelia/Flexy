9034fa77c055676d0f8773ff8e640558c3022d22
lnormandon <lnormandon@openstudio.fr>
2024-04-17 16:00:51 +0200

    Add button quantity

--- components/Molecules/Button/Button.stories.js
+++ components/Molecules/Button/Button.stories.js
@@ -1,10 +1,11 @@
-import Compact from './Compact.twig';
+import Compact from './Classic.twig';
+import Quantity from './Quantity.twig';
 
 export default {
   title: 'Design System/Molecules/Button'
 };
 
-export const compact = {
+export const classic = {
   render: (args) => Compact(args),
   args: {
     customText: 'Button',
@@ -33,3 +34,31 @@ export const compact = {
     }
   }
 };
+
+export const quantity = {
+  render: () => Quantity(),
+  play: ({ canvasElement }) => {
+    const minusButtons = canvasElement.querySelectorAll(
+      'button[data-qty="minus"]'
+    );
+    const plusButtons = canvasElement.querySelectorAll(
+      'button[data-qty="plus"]'
+    );
+    minusButtons.forEach((minus) => {
+      minus.addEventListener('click', (e) => plusMinusInput(minus, true));
+    });
+    plusButtons.forEach((plus) => {
+      plus.addEventListener('click', () => plusMinusInput(plus));
+    });
+  }
+};
+
+function plusMinusInput(el, minus = false) {
+  const input = el.parentElement.querySelector('input');
+  const value = parseInt(input.value);
+  if (minus) {
+    input.value = (value - 1).toString();
+    return;
+  }
+  input.value = (value + 1).toString();
+}
