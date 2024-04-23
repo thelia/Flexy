6a8bf1a0910616e97e1455579e6c5201928f1eeb
mbecle <mbecle@openstudio.fr>
2024-04-23 15:04:02 +0200

    feat: tooltip

---
+++ components/Molecules/Tooltip/Tooltip.stories.js
@@ -0,0 +1,21 @@
+import Tooltip from './Tooltip.twig';
+
+export default {
+  title: 'Design System/Molecules/Tooltip'
+};
+
+export const Base = {
+  render: (args) => Tooltip(args),
+  args: {
+    tooltip: "Ici un texte pour le tool tip, celui-ci doit rester en dessous de 80 caractères.",
+  },
+  argTypes: {
+    position: {
+      options: ['top', 'bottom'],
+      control: { type: 'select' },
+    }
+  },
+  parameters: {
+    layout: 'centered',
+  },
+};
