81751f44d58c08af942b33f4c869e94e79728237
mbecle <mbecle@openstudio.fr>
2024-04-26 11:47:31 +0200

    feat: dropdown

--- components/Molecules/Filters/FilterSelect/FilterSelect.stories.js
+++ components/Molecules/Filters/FilterSelect/FilterSelect.stories.js
@@ -7,8 +7,9 @@ export default {
 export const Base = {
   render: (args) => FilterSelect(args),
   args: {
-    id: "FilterSelect",
-    options: [{ id: 1, value: 1, label: "S - 34/36" }, { id: 2, value: 2, label: "M - 38/40" }, { id: 3, value: 3, label: "L - 42/44" }],
+    id: "filterSelect",
+    placeholder: 'Taille',
+    options: [{ value: 1, label: "S - 34/36" }, { value: 2, label: "M - 38/40" }, { value: 3, label: "L - 42/44" }],
   }
 };
 
@@ -16,6 +17,6 @@ export const Color = {
   render: (args) => FilterSelect(args),
   args: {
     id: "colorFilterSelect",
-    options: [{ id: 1, value: 1, label: "<span class='colorRounded' data-bg-color='#6969B3'></span>State Blue" }, { id: 2, value: 2, label: "<span class='colorRounded' data-bg-color='#FFB698'></span>Vermillon" }, { id: 3, value: 3, label: "<span class='colorRounded' data-bg-color='#767676'></span>Grey" }],
+    options: [{ value: 1, label: "<span class='colorRounded' data-bg-color='#6969B3'></span>State Blue" }, { value: 2, label: "<span class='colorRounded' data-bg-color='#FFB698'></span>Vermillon" }, { value: 3, label: "<span class='colorRounded' data-bg-color='#767676'></span>Grey" }],
   }
 };
