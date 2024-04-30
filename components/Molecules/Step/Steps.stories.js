4c33ffae32a8ba2a17652c0c95b317392f12a8cc
Achille Jouguelet <ajouguelet@openstudio.fr>
2024-04-30 15:41:17 +0200

    add steps molecules

---
+++ components/Molecules/Step/Steps.stories.js
@@ -0,0 +1,33 @@
+import Steps from './Steps.twig';
+
+export default {
+  title: 'Design System/Molecules/Steps'
+};
+
+export const base = {
+  render: (args) => Steps(args),
+  args: {
+    currentStep: 2,
+    checkoutSteps: {
+      CART: {
+        id: 1,
+        label: 'Votre panier'
+      },
+      DELIVERY: {
+        id: 2,
+        label: 'Votre livraison'
+      },
+      PAYMENT: {
+        id: 3,
+        label: 'Paiement'
+      },
+      CONFIRMATION: {
+        id: 4,
+        label: 'Confirmation'
+      }
+    },
+    cartTotal: '850,00€',
+    cartQuantity: '2 articles'
+  },
+  parameters: {}
+};
