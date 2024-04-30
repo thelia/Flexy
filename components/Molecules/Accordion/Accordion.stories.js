f3ad4f82bed4691f40e8802fa678745be4dfacc1
mbecle <mbecle@openstudio.fr>
2024-04-30 16:54:17 +0200

    feat: faq

--- components/Molecules/Accordion/Accordion.stories.js
+++ components/Molecules/Accordion/Accordion.stories.js
@@ -12,13 +12,13 @@ const commonProps = {
   },
   argTypes: {
     variant: {
-      options: ['text', 'bold', 'vermillon'],
+      options: ['text', 'promo', 'filter', 'faq'],
       control: { type: 'select' },
     },
   },
 };
 
-export const Base = {
+export const Text = {
   render: (args) => Accordion(args),
   ...commonProps,
   args: {
@@ -29,25 +29,35 @@ export const Base = {
 };
 
 // TODO: manque input avec bouton à l'intérieur pour le code promo
-export const Bold = {
+export const CodePromo = {
   render: (args) => Accordion(args),
   args: {
     ...commonProps.args,
-    variant: 'bold',
+    variant: 'promo',
     content: `${FieldInput({ placeholder: 'Code Promo', name: 'promoCode' })}`,
   }
 };
 
-export const Vermillon = {
+export const Filter = {
   render: (args) => Accordion(args),
   args: {
     ...commonProps.args,
-    variant: 'vermillon',
+    variant: 'filter',
     content: `
     ${FilterList({ label: 'Du - cher au + cher', value: 1 })}
     ${FilterList({ label: 'Du + cher au - cher', value: 2 })}
     ${FilterList({ label: 'Nouveautés', value: 3 })}
     ${FilterList({ label: 'Meilleurs notes', value: 4 })}
     ${FilterList({ label: 'Promotions', value: 5 })}`,
+  }
+};
+
+export const Faq = {
+  render: (args) => Accordion(args),
+  ...commonProps,
+  args: {
+    title: "Une question fréquente ici ?",
+    variant: 'faq',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   }
 };
