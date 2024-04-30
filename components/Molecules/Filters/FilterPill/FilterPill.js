9ee6f3466bc69be70cedeca6bfa6cc5e59f7371d
mbecle <mbecle@openstudio.fr>
2024-04-30 12:23:31 +0200

    fix: close dropdown & filterSelect

--- components/Molecules/Filters/FilterPill/FilterPill.js
+++ components/Molecules/Filters/FilterPill/FilterPill.js
@@ -1,6 +1,6 @@
 const filterPillFunction = () => {
   const close = document.querySelector('.FilterPill--selected');
-  console.log('pill ok');
+
   if (close) {
     close.addEventListener('click', function () {
       this.parentNode.style.display = 'none';
