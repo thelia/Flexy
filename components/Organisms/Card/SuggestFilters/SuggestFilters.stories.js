998d6ba09b103130c722de572ef520819c2d60cd
mbecle <mbecle@openstudio.fr>
2024-05-02 15:37:33 +0200

    feat: card suggest filters

---
+++ components/Organisms/Card/SuggestFilters/SuggestFilters.stories.js
@@ -0,0 +1,13 @@
+import SuggestFilters from './SuggestFilters.twig';
+
+export default {
+  title: 'Design System/Organisms/Card/SuggestFilters'
+};
+
+export const Base = {
+  render: (args) => SuggestFilters(args),
+  args: {
+    customText: "<p>Vous n'avez pas trouvé le produit que vous recherchez ?</p><p>Essayez les filtres</p>",
+    button: "Filtrer & trier",
+  }
+};
