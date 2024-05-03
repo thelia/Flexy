34c8fa1f808fb395b196b0ea0f647c311bce5af6
lnormandon <lnormandon@openstudio.fr>
2024-05-03 16:18:59 +0200

    Add Small Reinsurance

---
+++ components/Organisms/Reinsurance/Reinsurance.stories.js
@@ -0,0 +1,26 @@
+import Reinsurance from './Reinsurance.twig';
+
+export default {
+  title: 'Design System/Organisms/Reinsurance'
+};
+
+export const small = {
+  render: (args) => Reinsurance(args),
+  args: {
+    data: [
+      {
+        strongText: 'Livraison gratuite',
+        text: ' à partir de 50€',
+        icon: 'reinsurance-delivery'
+      },
+      {
+        strongText: 'Expédition',
+        text: ' sous 24h',
+        icon: 'reinsurance-shipping'
+      }
+    ]
+  },
+  argTypes: {
+    data: { type: 'array' }
+  }
+};
