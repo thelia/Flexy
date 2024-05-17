062f74efe325ff1bd218ba6b2e8876e620b57c0a
lnormandon <lnormandon@openstudio.fr>
2024-05-17 15:02:34 +0200

    SnackBar

---
+++ components/Organisms/SnackBar/SnackBar.stories.js
@@ -0,0 +1,26 @@
+import SnackBar from './SnackBar.twig';
+
+export default {
+  title: 'Design System/Organisms/SnackBar'
+};
+
+export const base = {
+  render: (args) => SnackBar(args),
+  args: {
+    size:  'small',
+    variant: 'error',
+    button: 'Bouton',
+    text: 'Ici un message d’erreur pour aider l’utilisateur à corriger son erreur.',
+    withIcon: true
+  },
+  argTypes: {
+    size: {
+      options: ['small', 'large'],
+      control: { type: 'radio' }
+    },
+    variant: {
+      options: ['error', 'warning', 'validated', 'informative', 'neutral-light', 'neutral-dark'],
+      control: { type: 'radio' }
+    },
+  }
+};
