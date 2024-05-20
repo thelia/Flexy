24627d9846986ec81e2ac02c0f3b381239235925
lnormandon <lnormandon@openstudio.fr>
2024-05-20 15:29:40 +0200

    Address Card - Client information

--- components/Organisms/Card/Address/AddressCard.stories.js
+++ components/Organisms/Card/Address/AddressCard.stories.js
@@ -1,8 +1,20 @@
 import AddressCard from './AddressCard.twig';
+import ClientAddressCard from './ClientAddressCard.twig';
 import { addressCard } from './AddressCard';
 
 export default {
   title: 'Design System/Organisms/Card/AddressCard'
+};
+
+const address = {
+  title: "Domicile",
+    name: "Eleanor Shellstrop",
+    address1: "12 rue du port",
+    address2: "Bâtiment C",
+    zipCode: '63000',
+    city: 'Clermont-Ferrand',
+    country: 'France',
+    phone: '06 00 00 00 00',
 };
 
 export const Base = {
@@ -12,16 +24,7 @@ export const Base = {
   },
   args: {
     purchaseFunnel: false,
-    address: {
-      title: "Domicile",
-      name: "Eleanor Shellstrop",
-      address1: "12 rue du port",
-      address2: "Bâtiment C",
-      zipCode: '63000',
-      city: 'Clermont-Ferrand',
-      country: 'France',
-      phone: '06 00 00 00 00',
-    },
+    address: address,
     isDefault: true,
     selected: true,
     radio: false,
@@ -30,5 +33,12 @@ export const Base = {
     radio: {if: { arg: 'purchaseFunnel', truthy: false }},
     selected: {if: { arg: 'purchaseFunnel', truthy: false }},
     isDefault: {if: { arg: 'purchaseFunnel', truthy: false }},
+  }
+};
+export const ClientInformation = {
+  render: (args) => `<div class='max-w-[540px]'>${ClientAddressCard(args)}</div>`,
+  args: {
+    address: address,
+    undefined: false
   }
 };
