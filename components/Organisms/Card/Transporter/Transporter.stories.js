0eee25dc698dc02f406ad437f1e8bedcfca92e44
lnormandon <lnormandon@openstudio.fr>
2024-05-20 16:33:05 +0200

    Transporter

---
+++ components/Organisms/Card/Transporter/Transporter.stories.js
@@ -0,0 +1,16 @@
+import Transporter from './Transporter.twig';
+
+export default {
+  title: 'Design System/Organisms/Card/Transporter'
+};
+
+export const Base = {
+  render: (args) => `<div class='w-[340px]'>${Transporter(args)}</div>`,
+  args: {
+    selected: false,
+    title: 'Nom du transporteur',
+    date: 'JJ/MM',
+    price: '7,80€',
+    img: {alt: 'Logo Chronopost', src: "/images/chronopost.svg"}
+  }
+};
