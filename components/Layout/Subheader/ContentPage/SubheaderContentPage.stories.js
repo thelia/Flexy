075ef635bfa17c77e5951e43169aff87bdc4bbb3
lnormandon <lnormandon@openstudio.fr>
2024-06-04 10:57:16 +0200

    Subheader content page

---
+++ components/Layout/Subheader/ContentPage/SubheaderContentPage.stories.js
@@ -0,0 +1,16 @@
+import SubheaderContentPage from './SubheaderContentPage.twig';
+
+export default {
+  title: 'Design System/Layout/Subheader/Content Page'
+};
+
+export const base = {
+  render: (args) => SubheaderContentPage(args),
+  args: {
+    title: 'Ici le titre de l’article',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    img: { alt: 'image 1', src: '/images/placeholder2.webp' }
+  },
+  argTypes: {}
+};
