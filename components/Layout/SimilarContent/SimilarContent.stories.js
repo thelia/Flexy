0632e80bcf40d28dade7cf06bec95a1455d99e56
lnormandon <lnormandon@openstudio.fr>
2024-06-07 13:54:03 +0200

    Similar content

---
+++ components/Layout/SimilarContent/SimilarContent.stories.js
@@ -0,0 +1,32 @@
+import SimilarContent from './SimilarContent.twig';
+import { similarContent } from './similarContent';
+
+export default {
+  title: 'Design System/Layout/SimilarContent'
+};
+
+const standardSimilarContent = {
+  title: 'Titre de l’article à cet endroit',
+  date: 'JJ.MM.AAAA'
+};
+
+const similarContents = [];
+
+for (let i = 0; i < 3; i++) {
+  similarContents.push(standardSimilarContent);
+}
+
+export const base = {
+  render: (args) => SimilarContent(args),
+  args: {
+    title: 'Sur le même sujet',
+    similarContents,
+    button: { href: '#', label: 'Voir toutes les actualités' }
+  },
+  play: () => {
+    similarContent();
+  },
+  parameters: {
+    backgrounds: { default: 'grey' }
+  }
+};
---
+++ components/Organisms/SimilarContentCard/SimilarContentCard.stories.js
@@ -0,0 +1,16 @@
+import SimilarContentCard from './SimilarContentCard.twig';
+
+export default {
+  title: 'Design System/Organisms/SimilarContentCard'
+};
+
+export const base = {
+  render: (args) => SimilarContentCard(args),
+  args: {
+    title: 'Titre de l’article à cet endroit',
+    date: 'JJ.MM.AAAA'
+  },
+  parameters: {
+    backgrounds: { default: 'grey' }
+  }
+};
