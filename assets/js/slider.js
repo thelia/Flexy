74920938eb5d4e79430e40871aa11f3aeb29b894
lnormandon <lnormandon@openstudio.fr>
2024-08-02 16:17:17 +0200

    fix slider js : add mouse controls

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,31 +1,44 @@
 export function slider() {
-  const slider = document.querySelector('.slider');
+  const sliders = document.querySelectorAll('.slider');
   let isDown = false;
   let startX;
   let scrollLeft;
 
-  slider.addEventListener('mousedown', (e) => {
+  sliders.forEach(slider => {
+    slider.addEventListener('mousedown', (e) => start(e.pageX, slider));
+    slider.addEventListener('touchstart', (e) => start(e.touches[0].pageX, slider));
+
+    slider.addEventListener('mouseleave', () => end(slider));
+    slider.addEventListener('touchend', () => end(slider));
+
+    slider.addEventListener('mouseup', () => end(slider));
+    slider.addEventListener('touchcancel', () => end(slider));
+
+    slider.addEventListener('mousemove', (e) => move(e, e.pageX, slider));
+    slider.addEventListener('touchmove', (e) => move(e, e.touches[0].pageX, slider));
+  });
+
+
+
+  function start(pageX,slider)
+  {
     isDown = true;
     slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
+    startX = pageX - slider.offsetLeft;
     scrollLeft = slider.scrollLeft;
-  });
-
-  slider.addEventListener('mouseleave', () => {
+  }
+  function end(slider)
+  {
     isDown = false;
     slider.classList.remove('active');
-  });
-
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-
-  slider.addEventListener('mousemove', (e) => {
+  }
+  function move(e, pageX, slider)
+  {
     if (!isDown) return;
     e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
+    const x = pageX - slider.offsetLeft;
     const walk = (x - startX) * 3; // Adjust the scroll speed here
     slider.scrollLeft = scrollLeft - walk;
-  });
+  }
+
 }
