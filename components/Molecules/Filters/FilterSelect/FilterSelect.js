b4823c56ad63b230491027a1ca7f8beba6bc49f0
lnormandon <lnormandon@openstudio.fr>
2024-08-09 17:30:49 +0200

    Fixs selects #1

--- components/Molecules/Filters/FilterSelect/FilterSelect.js
+++ components/Molecules/Filters/FilterSelect/FilterSelect.js
@@ -11,39 +11,39 @@ const filterSelectFunction = () => {
     });
   }
 
-  let focusActive = false;
-  const currentOption = document.querySelector(".FilterSelect-current");
-  const options = document.querySelectorAll(".FilterSelect-option");
-  const liOptions = document.querySelectorAll(".FilterSelect-listItem");
-
-  // Supprimer le focus de l'option pour refermer le filtre
-  options.forEach(function (option) {
-    option.addEventListener("click", function () {
-      if (focusActive) {
-        option.blur();
-      }
-    });
-  });
-
-  // Navigation clavier
-  liOptions.forEach(function (liOption) {
-    liOption.addEventListener("keypress", function (event) {
-      if (event.key === "Enter") {
-        liOption.querySelector('label').blur();
-        const inputId = liOption.querySelector('label').getAttribute("for");
-        const correspondingInput = document.getElementById(inputId);
-        correspondingInput.checked = true;
-      }
-    });
-  });
-
-  // Refermer le filtre si on reclic sur l'option currente (en-tête)
-  currentOption.addEventListener("click", function () {
-    if (focusActive) {
-      currentOption.blur();
-    };
-    focusActive = !focusActive;
-  });
+  // let focusActive = false;
+  // const currentOption = document.querySelector(".FilterSelect-current");
+  // const options = document.querySelectorAll(".FilterSelect-option");
+  // const liOptions = document.querySelectorAll(".FilterSelect-listItem");
+  //
+  // // Supprimer le focus de l'option pour refermer le filtre
+  // options.forEach(function (option) {
+  //   option.addEventListener("click", function () {
+  //     if (focusActive) {
+  //       option.blur();
+  //     }
+  //   });
+  // });
+  //
+  // // Navigation clavier
+  // liOptions.forEach(function (liOption) {
+  //   liOption.addEventListener("keypress", function (event) {
+  //     if (event.key === "Enter") {
+  //       liOption.querySelector('label').blur();
+  //       const inputId = liOption.querySelector('label').getAttribute("for");
+  //       const correspondingInput = document.getElementById(inputId);
+  //       correspondingInput.checked = true;
+  //     }
+  //   });
+  // });
+  //
+  // // Refermer le filtre si on reclic sur l'option currente (en-tête)
+  // currentOption.addEventListener("click", function () {
+  //   if (focusActive) {
+  //     currentOption.blur();
+  //   };
+  //   focusActive = !focusActive;
+  // });
 }
 
 export default filterSelectFunction;
