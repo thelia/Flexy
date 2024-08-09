3c5cd9c3c955c030abc3dcb438fd1fc2aef110b5
jordi hermes babalako <jhbabalako@openstudio.fr>
2024-08-09 16:32:25 +0200

    Component Hero

---
+++ components/Layout/Hero/Hero.stories.js
@@ -0,0 +1,37 @@
+import Hero from './Hero.twig';
+
+export default {
+  title: 'Design System/Layout/Hero'
+};
+
+
+
+export const Base = {
+  render: (args) => Hero(args),
+  args: {
+    blocks: [
+      {
+        image: 'https://sabatier-k.openstudio-lab.com/cache/images/carousel/design780x480-4-4.png',
+        title: 'Ici une phrase d’accroche pour accompagner le visuel',
+        href: 'http://sabatier-k.openstudio-lab.com/couteaux-sabatier.html',
+        linkLabel: 'Je découvre'
+      },
+      {
+        image: '',
+        title: 'Ici une phrase d’accroche pour accompagner le visuel',
+        href: 'http://example.com',
+        linkLabel: 'Je découvre'
+      },
+      {
+        image: '',
+        title: 'Ici une phrase d’accroche pour accompagner le visuel',
+        href: 'http://example.com',
+        linkLabel: 'Je découvre'
+      }
+    ]
+  },
+  argTypes: {
+    blocks: { control: 'object' },
+  }
+};
+
