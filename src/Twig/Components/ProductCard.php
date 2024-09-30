3819c4d38fd2035103c90ce83658ded6eec3c314
Robin Morel <rmorel@openstudio.fr>
2024-09-30 11:53:42 +0200

    ProductCard

--- src/Twig/Components/ProductCard.php
+++ src/Twig/Components/ProductCard.php
@@ -4,13 +4,16 @@ namespace FlexyBundle\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use TwigEngine\Service\DataAccess\DataAccessService;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
 
 #[AsTwigComponent(template: '@components/Organisms/ProductCard/ProductCard.html.twig')]
 class ProductCard
 {
   private DataAccessService $dataAccessService;
-  public ?string $productId;
-  public ?array $product;
+  public ?string $productId = '';
+
+  #[ExposeInTemplate]
+  public ?array $product = null;
 
   public function __construct(DataAccessService $dataAccessService)
   {
@@ -19,9 +22,16 @@ class ProductCard
 
   public function getProduct()
   {
+    if (null !== $this->product) {
+      return $this->product;
+    }
+
     if ('' === $this->productId) {
       return null;
     }
-    return $this->dataAccessService->resources('/api/front/products/' . $this->productId);
+
+    $this->product = $this->dataAccessService->resources('/api/front/products/' . $this->productId);
+
+    return $this->product;
   }
 }
