71ef78cd3a74f2bb0a7b617d72c899cad22596e2
Robin Morel <rmorel@openstudio.fr>
2024-10-01 15:54:04 +0200

    Live PseSelector Component

--- src/Twig/Layout/PseSelector.php
+++ src/Twig/Layout/PseSelector.php
@@ -16,37 +16,101 @@ use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use TwigEngine\Service\DataAccess\DataAccessService;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
-use Thelia\Api\Resource\Product;
+use TwigEngine\Service\DataAccess\ProductSaleElementsAccessService;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
 
 #[AsLiveComponent(template: '@components/Layout/PseSelector/PseSelector.html.twig')]
 class PseSelector
 {
   use DefaultActionTrait;
 
+  #[LiveProp]
   public array $product;
 
+  #[ExposeInTemplate]
+  public ?array $pses = [];
+
+  #[ExposeInTemplate]
+  public ?array $currentPse = null;
+
+
   #[LiveProp(writable: true)]
-  public ?string $pseId = null;
+  #[ExposeInTemplate]
+  public ?array $currentCombination = null;
+
+  #[ExposeInTemplate]
+  public ?array $productAttributes = [];
 
   private DataAccessService $dataAccessService;
+  private readonly ProductSaleElementsAccessService $pseAccessService;
 
-  public function __construct(DataAccessService $dataAccessService)
+  public function __construct(DataAccessService $dataAccessService, ProductSaleElementsAccessService $pseAccessService)
   {
     $this->dataAccessService = $dataAccessService;
+    $this->pseAccessService = $pseAccessService;
   }
 
-  public function pse(): array
+  public function getPses(): array
   {
-    if ('' === $this->pseId) {
+    if (0 !== count($this->pses)) {
+      return $this->pses;
+    }
+
+    $this->pses = json_decode($this->pseAccessService->psesByProduct($this->product['id']), true);
+
+    return $this->pses;
+  }
+
+  public function getProductAttributes(): array
+  {
+    if (0 !== count($this->productAttributes)) {
+      return $this->productAttributes;
+    }
+
+    $this->productAttributes = $this->pseAccessService->attrAvByProduct($this->product['id']);
+
+    return $this->productAttributes;
+  }
+
+  public function getCurrentPse()
+  {
+    if (null !== $this->currentPse) {
+      return $this->currentPse;
+    }
+
+    $pses = $this->getPses();
+
+    if (0 === count($pses)) {
       return [];
     }
-    return $this->dataAccessService->resources('/api/front/product_sale_elements/' . $this->pseId);
+
+    if (null !== $this->currentCombination) {
+      $matchedPse =  array_filter($pses, function ($pse) {
+        return $pse['combination'] == $this->currentCombination;
+      });
+      $this->currentPse = reset($matchedPse);
+    } else {
+
+      $this->currentPse =  array_filter($pses, function ($pse) {
+        return $pse['isDefault'];
+      })[0];
+    }
+
+    return $this->currentPse;
   }
 
-  public function defaultPse(): array
+  public function getCurrentCombination()
   {
-    return array_filter($this->product['productSaleElements'], function ($pse) {
-      return $pse['isDefault'];
-    })[0];
+    if (null !== $this->currentCombination) {
+      return $this->currentCombination;
+    }
+
+    if (null === $this->currentPse) {
+      return null;
+    }
+
+    $this->currentCombination = $this->currentPse["combination"];
+
+    return $this->currentCombination;
   }
 }
